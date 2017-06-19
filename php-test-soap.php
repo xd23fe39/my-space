6d44942000aec0942fb1a49be46b27ac95c17cab
Frank <xd23fe39@yahoo.de>
2017-06-19 09:59:07 +0200

    Update php-test-soap.php

--- php-test-soap.php
+++ php-test-soap.php
@@ -2,17 +2,17 @@
 <?php
         function testSoap() {
 
-                $wsdl_url = "http://timprod.noc.fiducia.de:8765/service?wsdl";
-                $search = "*1550*";
+                $wsdl_url = "http://server:8765/service?wsdl";
+                $search = "*1234*";
 
                 try {
                         $client = new SoapClient($wsdl_url , array());
 
                         /* $client = new SoapClient($wsdl_url , array(
-                                        'proxy_host'     => "inetgw1",
+                                'proxy_host'     => "proxy",
                                 'proxy_port'     => 8080));  */
 
-                        $i = $client->createContext("DFSREGSDEGHEDJDDHGHZXVS32");
+                        $i = $client->createContext("XXXXXXXXXEGHEDJDDHGHZXVS32");
                         $res_str = $client->searchPerson("(employeenumber=*$search*)");
 
                         echo $res_str;
